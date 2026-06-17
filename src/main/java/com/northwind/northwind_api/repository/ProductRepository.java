2aa67f1fc34a3b573f4100fef86a6d4d0f429736
colocky <callmeadam.ar@gmail.com>
2026-06-17 11:10:55 -0500

    Add Category Query to ProductRepository.java

--- src/main/java/com/northwind/northwind_api/repository/ProductRepository.java
+++ src/main/java/com/northwind/northwind_api/repository/ProductRepository.java
@@ -2,8 +2,13 @@ package com.northwind.northwind_api.repository;
 
 import com.northwind.northwind_api.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer>{
+    @Query("SELECT p FROM Product p LEFT JOIN FETCH p.category")
+    List<Product> findAllWithCategory();
 }
