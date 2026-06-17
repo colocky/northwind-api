e857de9fbab6ee9cfd19b534b199b02d64800933
colocky <callmeadam.ar@gmail.com>
2026-06-17 11:15:05 -0500

    Fix CategoryId not being identified as Primary Key in Category.java

--- src/main/java/com/northwind/northwind_api/model/Category.java
+++ src/main/java/com/northwind/northwind_api/model/Category.java
@@ -1,8 +1,6 @@
 package com.northwind.northwind_api.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table (name = "Category")
@@ -11,6 +9,8 @@ public class Category {
     @Column (name = "CategoryName")
     private String categoryName;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column (name = "CategoryId")
     private Integer categoryId;
 
