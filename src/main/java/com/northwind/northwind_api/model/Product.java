57f65aba9f6473f14d95a0ee4ab34d1b6ba2eb89
colocky <callmeadam.ar@gmail.com>
2026-06-17 11:08:31 -0500

    Fix Category Getter/Setter in Product.java

--- src/main/java/com/northwind/northwind_api/model/Product.java
+++ src/main/java/com/northwind/northwind_api/model/Product.java
@@ -103,10 +103,10 @@ public class Product {
     public void setDiscontinued(Boolean discontinued) {
         this.discontinued = discontinued;
     }
-    public String getCategory() {
+    public Category getCategory() {
         return category;
     }
-    public void setCategory(String category) {
+    public void setCategory(Category category) {
         this.category = category;
     }
 }
