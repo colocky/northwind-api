6e8cf1d136bfc18f950cdcb11d01e07ef1a20b63
colocky <callmeadam.ar@gmail.com>
2026-06-17 13:58:49 -0500

    Add Order Model

---
+++ src/main/java/com/northwind/northwind_api/model/Order.java
@@ -0,0 +1,101 @@
+package com.northwind.northwind_api.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "OrderID")
+    private Integer orderId;
+
+    @Column(name = "CustomerID")
+    private Integer customerId;
+
+    @Column(name = "EmployeeID")
+    private Integer employeeId;
+
+    @Column(name = "OrderDate")
+    private LocalDate orderDate;
+
+    @Column(name = "RequiredDate")
+    private LocalDate requiredDate;
+
+    @Column(name = "ShippedDate")
+    private LocalDate shippedDate;
+
+    @Column(name = "ShipVia")
+    private Integer shipVia;
+
+    @Column(name ="Freight")
+    private BigDecimal freight;
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public LocalDate getRequiredDate() {
+        return requiredDate;
+    }
+
+    public void setRequiredDate(LocalDate requiredDate) {
+        this.requiredDate = requiredDate;
+    }
+
+    public LocalDate getShippedDate() {
+        return shippedDate;
+    }
+
+    public void setShippedDate(LocalDate shippedDate) {
+        this.shippedDate = shippedDate;
+    }
+
+    public Integer getShipVia() {
+        return shipVia;
+    }
+
+    public void setShipVia(Integer shipVia) {
+        this.shipVia = shipVia;
+    }
+
+    public BigDecimal getFreight() {
+        return freight;
+    }
+
+    public void setFreight(BigDecimal freight) {
+        this.freight = freight;
+    }
+}
