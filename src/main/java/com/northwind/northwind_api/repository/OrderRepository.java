35683da9c1028fd610f6cc794d9982b8f79c4691
colocky <callmeadam.ar@gmail.com>
2026-06-17 14:05:55 -0500

    Add OrderRepository

---
+++ src/main/java/com/northwind/northwind_api/repository/OrderRepository.java
@@ -0,0 +1,14 @@
+package com.northwind.northwind_api.repository;
+
+import com.northwind.northwind_api.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+    List<Order> findByCustomerId(Integer customerId);
+    List<Order> findByEmployeeId(Integer employeeId);
+}
