3f6bbba9ec22cdd368fb49512b2f5e02f9b5c7b9
colocky <callmeadam.ar@gmail.com>
2026-06-18 11:41:43 -0500

    Add Customer Model

---
+++ src/main/java/com/northwind/northwind_api/model/Customer.java
@@ -0,0 +1,65 @@
+package com.northwind.northwind_api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Customers")
+public class Customer {
+
+    @Column(name = "CustomerID")
+    private String customerId;
+
+    @Column(name = "CompanyName")
+    private String companyName;
+
+    @Column(name = "ContactName")
+    private String contactName;
+
+    @Column(name = "Country")
+    private String country;
+
+    @Column(name = "City")
+    private String city;
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
