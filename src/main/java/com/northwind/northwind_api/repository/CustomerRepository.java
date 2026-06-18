d8b80fb27a792e0c12c63bdda07d367e106b5a1f
colocky <callmeadam.ar@gmail.com>
2026-06-18 11:43:21 -0500

    Add CustomerRepository

---
+++ src/main/java/com/northwind/northwind_api/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.northwind.northwind_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository {
+
+}
