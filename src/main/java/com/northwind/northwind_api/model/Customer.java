d1c1aba37125e74355adfb1b6bf631768dbfb2d3
colocky <callmeadam.ar@gmail.com>
2026-06-18 11:47:19 -0500

    Add @Id annotation to customerId in Customer.java Fix CustomerRepository

--- src/main/java/com/northwind/northwind_api/model/Customer.java
+++ src/main/java/com/northwind/northwind_api/model/Customer.java
@@ -2,12 +2,14 @@ package com.northwind.northwind_api.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Customers")
 public class Customer {
 
+    @Id
     @Column(name = "CustomerID")
     private String customerId;
 
--- src/main/java/com/northwind/northwind_api/repository/CustomerRepository.java
+++ src/main/java/com/northwind/northwind_api/repository/CustomerRepository.java
@@ -1,9 +1,10 @@
 package com.northwind.northwind_api.repository;
 
+import com.northwind.northwind_api.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends JpaRepository {
+public interface CustomerRepository extends JpaRepository<Customer, String> {
 
 }
